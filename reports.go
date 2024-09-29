1ef0e676a18c9b6ab48f4835b191607390d81f15
egerong <egert@isiro.ee>
2024-09-29 22:14:24 +0300

    Use decimal in reports

--- reports.go
+++ reports.go
@@ -3,6 +3,8 @@ package merit
 import (
 	"fmt"
 	"time"
+
+	"github.com/shopspring/decimal"
 )
 
 // GetPurchaseReportQuery represents the query parameters for retrieving purchase reports.
@@ -75,24 +77,24 @@ func (query GetPurchaseReportQuery) format(repType reportType) getPurchaseReport
 }
 
 type PurchaseReportByInvoice struct {
-	DocId          string  `json:"docId"`
-	InvoiceNo      string  `json:"invoiceNo"`
-	CurrencyCode   string  `json:"currencyCode"`
-	CurrencyRate   float64 `json:"currencyRate"`
-	VendorId       string  `json:"vendorId"`
-	VendorName     string  `json:"vendorName"`
-	RegNo          string  `json:"regNo"`
-	VatRegNo       string  `json:"vatRegNo"`
-	InvoiceDate    string  `json:"invoiceDate"`
-	Amount         float64 `json:"amount"`
-	VatAmount      float64 `json:"vatAmount"`
-	RoundingAmount float64 `json:"roundingAmount"`
-	TotalAmount    float64 `json:"totalAmount"`
-	ExpenseClaim   bool    `json:"expenseClaim"`
-	BatchId        string  `json:"batchId"`
-	BatchCode      string  `json:"batchCode"`
-	BatchNo        int     `json:"batchNo"`
-	Ctry           string  `json:"ctry"`
+	DocId          string          `json:"docId"`
+	InvoiceNo      string          `json:"invoiceNo"`
+	CurrencyCode   string          `json:"currencyCode"`
+	CurrencyRate   decimal.Decimal `json:"currencyRate"`
+	VendorId       string          `json:"vendorId"`
+	VendorName     string          `json:"vendorName"`
+	RegNo          string          `json:"regNo"`
+	VatRegNo       string          `json:"vatRegNo"`
+	InvoiceDate    string          `json:"invoiceDate"`
+	Amount         decimal.Decimal `json:"amount"`
+	VatAmount      decimal.Decimal `json:"vatAmount"`
+	RoundingAmount decimal.Decimal `json:"roundingAmount"`
+	TotalAmount    decimal.Decimal `json:"totalAmount"`
+	ExpenseClaim   bool            `json:"expenseClaim"`
+	BatchId        string          `json:"batchId"`
+	BatchCode      string          `json:"batchCode"`
+	BatchNo        int             `json:"batchNo"`
+	Ctry           string          `json:"ctry"`
 }
 
 func (c *Client) GetPurchaseReportByInvoice(query GetPurchaseReportQuery) ([]PurchaseReportByInvoice, error) {
@@ -110,22 +112,22 @@ func (c *Client) GetPurchaseReportByInvoice(query GetPurchaseReportQuery) ([]Pur
 }
 
 type PurchaseReportByVendor struct {
-	CustomerId      string  `json:"customerId"`
-	CurrencyCode    string  `json:"currencyCode"`
-	VendorName      string  `json:"vendorName"`
-	RegNo           string  `json:"regNo"`
-	VatRegNo        string  `json:"vatRegNo"`
-	Amount          float64 `json:"amount"`
-	CAmount         float64 `json:"cAmount"`
-	VatAmount       float64 `json:"vatAmount"`
-	RoundingAmount  float64 `json:"roundingAmount"`
-	TotalAmount     float64 `json:"totalAmount"`
-	CVatAmount      float64 `json:"cVatAmount"`
-	CRoundingAmount float64 `json:"cRoundingAmount"`
-	CTotalAmount    float64 `json:"cTotalAmount"`
-	LnCnt           float64 `json:"lnCnt"`
-	DiscAmt         float64 `json:"discAmt"`
-	CDiscAmt        float64 `json:"cDiscAmt"`
+	CustomerId      string          `json:"customerId"`
+	CurrencyCode    string          `json:"currencyCode"`
+	VendorName      string          `json:"vendorName"`
+	RegNo           string          `json:"regNo"`
+	VatRegNo        string          `json:"vatRegNo"`
+	Amount          decimal.Decimal `json:"amount"`
+	CAmount         decimal.Decimal `json:"cAmount"`
+	VatAmount       decimal.Decimal `json:"vatAmount"`
+	RoundingAmount  decimal.Decimal `json:"roundingAmount"`
+	TotalAmount     decimal.Decimal `json:"totalAmount"`
+	CVatAmount      decimal.Decimal `json:"cVatAmount"`
+	CRoundingAmount decimal.Decimal `json:"cRoundingAmount"`
+	CTotalAmount    decimal.Decimal `json:"cTotalAmount"`
+	LnCnt           decimal.Decimal `json:"lnCnt"`
+	DiscAmt         decimal.Decimal `json:"discAmt"`
+	CDiscAmt        decimal.Decimal `json:"cDiscAmt"`
 }
 
 func (c *Client) GetPurchaseReportByVendor(query GetPurchaseReportQuery) ([]PurchaseReportByVendor, error) {
@@ -143,18 +145,18 @@ func (c *Client) GetPurchaseReportByVendor(query GetPurchaseReportQuery) ([]Purc
 }
 
 type PurchaseReportByArticle struct {
-	ItemId       string  `json:"itemId"`
-	ItemCode     string  `json:"itemCode"`
-	ItemName     string  `json:"itemName"`
-	ItemGrId     string  `json:"itemGrId"`
-	ItemGrName   string  `json:"itemGrName"`
-	CurrencyCode string  `json:"currencyCode"`
-	Unit         string  `json:"unit"`
-	Quantity     float64 `json:"quantity"`
-	Price        float64 `json:"price"`
-	Amount       float64 `json:"amount"`
-	UomID1       string  `json:"uomId1"`
-	UomID2       string  `json:"uomId2"`
+	ItemId       string          `json:"itemId"`
+	ItemCode     string          `json:"itemCode"`
+	ItemName     string          `json:"itemName"`
+	ItemGrId     string          `json:"itemGrId"`
+	ItemGrName   string          `json:"itemGrName"`
+	CurrencyCode string          `json:"currencyCode"`
+	Unit         string          `json:"unit"`
+	Quantity     decimal.Decimal `json:"quantity"`
+	Price        decimal.Decimal `json:"price"`
+	Amount       decimal.Decimal `json:"amount"`
+	UomID1       string          `json:"uomId1"`
+	UomID2       string          `json:"uomId2"`
 }
 
 func (c *Client) GetPurchaseReportByArticle(query GetPurchaseReportQuery) ([]PurchaseReportByArticle, error) {
@@ -172,17 +174,17 @@ func (c *Client) GetPurchaseReportByArticle(query GetPurchaseReportQuery) ([]Pur
 }
 
 type PurchaseReportByFixedAsset struct {
-	DocId        string  `json:"docId"`
-	FaId         string  `json:"faId"`
-	InventaryNo  string  `json:"inventaryNo"`
-	Name         string  `json:"name"`
-	CurrencyCode string  `json:"currencyCode"`
-	Quantity     float64 `json:"quantity"`
-	Price        float64 `json:"price"`
-	Amount       float64 `json:"amount"`
-	RemAmount    float64 `json:"remAmount"`
-	DocNo        string  `json:"docNo"`
-	DocDate      string  `json:"docDate"`
+	DocId        string          `json:"docId"`
+	FaId         string          `json:"faId"`
+	InventaryNo  string          `json:"inventaryNo"`
+	Name         string          `json:"name"`
+	CurrencyCode string          `json:"currencyCode"`
+	Quantity     decimal.Decimal `json:"quantity"`
+	Price        decimal.Decimal `json:"price"`
+	Amount       decimal.Decimal `json:"amount"`
+	RemAmount    decimal.Decimal `json:"remAmount"`
+	DocNo        string          `json:"docNo"`
+	DocDate      string          `json:"docDate"`
 }
 
 func (c *Client) GetPurchaseReportByFixedAsset(query GetPurchaseReportQuery) ([]PurchaseReportByFixedAsset, error) {
