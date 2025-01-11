91b5b02f27ec6685f2f946f413bcb56b570f91a7
egerong <egert@isiro.ee>
2025-01-11 18:43:08 +0200

    Add dimension filtering

--- dimension.go
+++ dimension.go
@@ -1,27 +1,51 @@
 package merit
 
+import (
+	"github.com/Microsoft/go-winio/pkg/guid"
+)
+
 type DimensionsObject struct {
-	ID         string `json:"Id"`
-	DimID      int    `json:"DimId"`
-	DimValueId string `json:"DimValueId"`
-	DimCode    string `json:"DimCode"`
+	DimID      int       `json:"DimId"`
+	DimValueId guid.GUID `json:"DimValueId"`
+	DimCode    string    `json:"DimCode"`
 }
 
 type Dimension struct {
-	DimID   int    `json:"DimId"`
-	DimName string `json:"DimName"`
-	ID      string `json:"Id"`
-	Code    string `json:"Code"`
-	Name    string `json:"Name"`
-	EndDate string `json:"EndDate"`
+	DimID   int       `json:"DimId"`
+	DimName string    `json:"DimName"`
+	ID      guid.GUID `json:"Id"`
+	Code    string    `json:"Code"`
+	Name    string    `json:"Name"`
+	EndDate string    `json:"EndDate"`
 }
 
-func (c *Client) GetDimensions() ([]Dimension, error) {
-	var query struct{}
+func (c *Client) GetDimensions(query Dimension) ([]Dimension, error) {
 	dimensions := []Dimension{}
-	err := c.post(epGetListOfDimensions, query, &dimensions)
+	err := c.post(epGetListOfDimensions, struct{}{}, &dimensions)
 	if err != nil {
 		return nil, err
 	}
-	return dimensions, nil
+	filteredDimensions := []Dimension{}
+	for _, dimension := range dimensions {
+		if query.DimID != 0 && dimension.DimID != query.DimID {
+			continue
+		}
+		if query.DimName != "" && dimension.DimName != query.DimName {
+			continue
+		}
+		if query.ID != guid.FromArray([16]byte{}) && dimension.ID != query.ID {
+			continue
+		}
+		if query.Code != "" && dimension.Code != query.Code {
+			continue
+		}
+		if query.Name != "" && dimension.Name != query.Name {
+			continue
+		}
+		if query.EndDate != "" && dimension.EndDate != query.EndDate {
+			continue
+		}
+		filteredDimensions = append(filteredDimensions, dimension)
+	}
+	return filteredDimensions, nil
 }
--- dimension_test.go
+++ dimension_test.go
@@ -3,10 +3,43 @@ package merit_test
 import (
 	"encoding/json"
 	"testing"
+
+	"github.com/Microsoft/go-winio/pkg/guid"
+	"github.com/egerong/merit-aktiva-go"
 )
 
-func TestGetListOfDimensions(t *testing.T) {
-	dimensions, err := testClient.GetDimensions()
+func TestGetDimensions(t *testing.T) {
+	dimensions, err := testClient.GetDimensions(merit.Dimension{})
+	if err != nil {
+		t.Error(err)
+	}
+	j, err := json.MarshalIndent(dimensions, "", "  ")
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(string(j))
+}
+
+func TestGetDimensionsByID(t *testing.T) {
+	g, _ := guid.FromString("")
+	dimensions, err := testClient.GetDimensions(merit.Dimension{
+		ID: g,
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	j, err := json.MarshalIndent(dimensions, "", "  ")
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(string(j))
+}
+
+func TestGetDimensionsByCode(t *testing.T) {
+	dimensions, err := testClient.GetDimensions(merit.Dimension{
+		DimName: "Projekt",
+		Code:    "RH233303",
+	})
 	if err != nil {
 		t.Error(err)
 	}
