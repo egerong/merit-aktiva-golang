b1cf96133ac8f9149d5805e05f1207821b81cea6
egerong <egert@isiro.ee>
2025-01-12 20:28:33 +0200

    Add inventory tests

---
+++ inventory_test.go
@@ -0,0 +1,36 @@
+package merit_test
+
+import (
+	"encoding/json"
+	"testing"
+	"time"
+
+	"github.com/egerong/merit-aktiva-go"
+)
+
+func TestInventoryMovements(t *testing.T) {
+	inventoryMovements, err := testClient.GetInventoryMovements(merit.GetInventoryMovementsQuery{})
+	if err != nil {
+		t.Error(err)
+	}
+	j, err := json.MarshalIndent(inventoryMovements, "", "  ")
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(string(j))
+}
+
+func TestInventoryMovementsLastWeek(t *testing.T) {
+	inventoryMovements, err := testClient.GetInventoryMovements(merit.GetInventoryMovementsQuery{
+		PeriodStart: time.Now().AddDate(0, 0, -21),
+		PeriodEnd:   time.Now(),
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	j, err := json.MarshalIndent(inventoryMovements, "", "  ")
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(string(j))
+}
